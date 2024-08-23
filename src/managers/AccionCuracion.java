27edd6265459d82ea157b9478bc12b2f37a7b706
Ger678 <germanalejandroaguirre678@gmail.com>
2024-08-23 17:16:04 -0300

    feat: Add input validation for item selection in AccionCuracion

--- src/managers/AccionCuracion.java
+++ src/managers/AccionCuracion.java
@@ -2,6 +2,7 @@ package managers;
 
 import models.Enemigo;
 import models.Jugador;
+import models.ResultadoUsoItem;
 import models.InventarioJugador;
 import models.Item;
 import managers.GestorInventario;
@@ -15,13 +16,13 @@ public class AccionCuracion implements AccionCombate {
         
         InventarioJugador inventario = jugador.getInventario();
         gestorCombate.getInterfaz().mostrarInventario();
-        String itemInput = gestorCombate.getInterfaz().pedirEntrada("Escribi el item que queres usar... ");
-        if (itemInput != "") {
-            gestorInventario.usarObjeto(jugador, inventario, itemInput);
-            
-            gestorCombate.getInterfaz().mostrarMensaje("Consumiste 1 de " + itemInput + " y te curaste vida. " + "Vida actual: " + jugador.getVida());
-        } else{
-            gestorCombate.getInterfaz().mostrarMensaje("No se encontro el item en tu inventario. Perdiste el turno.");
-        }
+        //Previene la entrada vacia
+        String itemInput = "";
+        do {
+            itemInput = gestorCombate.getInterfaz().pedirEntrada("Escribi el item que queres usar... ");
+        } while (itemInput.isEmpty());
+
+        ResultadoUsoItem resultado = gestorInventario.usarObjeto(jugador, inventario, itemInput);
+        gestorCombate.getInterfaz().mostrarMensaje(resultado.getMensaje());
     }
 }
