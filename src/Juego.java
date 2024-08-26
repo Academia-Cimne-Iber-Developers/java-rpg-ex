dd4b520d1cca52053e06bb9e84945fbbd3824357
Gabriel Nuñez <gabrielgusnunez2004@gmail.com>
2024-08-26 00:49:46 -0300

    Ya corregi los errores y conflictos que habian ahora voy hacer el siguiente PR a la main

--- src/Juego.java
+++ src/Juego.java
@@ -49,7 +49,7 @@ public class Juego {
         Ubicacion nuevaUbicacion = mapa.getUbicacion(destino);
         if (nuevaUbicacion != null) {
             String resultado = gestorExploracion.viajar(nuevaUbicacion);
-            interfaz.mostrarMensaje(resultado);
+            Interfaz.mostrarMensaje(resultado);
             interfaz.mostrarResultadoViaje(true, destino);
 
             // Ejemplo de agregar una nueva misión cuando el jugador llega a la "Cueva Profunda"
@@ -93,11 +93,11 @@ public class Juego {
 
                 interfaz.mostrarResultadoCombate("Has derrotado a " + enemigoDerrotado, gestorMisiones);
             } else {
-                interfaz.mostrarMensaje("Has sido derrotado. Fin del juego.");
+                Interfaz.mostrarMensaje("Has sido derrotado. Fin del juego.");
                 System.exit(0);
             }
         } else {
-            interfaz.mostrarMensaje("- No hay enemigos en esta ubicación.");
+            Interfaz.mostrarMensaje("- No hay enemigos en esta ubicación.");
         }
     }
 
@@ -131,16 +131,16 @@ public class Juego {
 
         // Manejamos el resultado y mostramos el mensaje que corresponde
         if (resultado.isExito()) {
-            interfaz.mostrarMensaje("Éxito: " + resultado.getMensaje());
+            Interfaz.mostrarMensaje("Éxito: " + resultado.getMensaje());
         } else {
-            interfaz.mostrarMensaje("Error: " + resultado.getMensaje());
+            Interfaz.mostrarMensaje("Error: " + resultado.getMensaje());
         }
         return resultado.getMensaje();
     }
 
     public void agregarNuevaMision(Mision nuevaMision) {
         gestorMisiones.agregarMision(nuevaMision);
-        interfaz.mostrarMensaje("¡Nueva misión obtenida: " + nuevaMision.getDescripcion() + "!");
+        Interfaz.mostrarMensaje("¡Nueva misión obtenida: " + nuevaMision.getDescripcion() + "!");
     }
 
     public static void main(String[] args) {
